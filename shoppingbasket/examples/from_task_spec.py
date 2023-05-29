493f07e6595fd7c3e54c287ea9461ce33188ec69
timbonator <me@timoates.com>
2023-05-29 18:09:49 +0100

    Added from_task_spec example

---
+++ shoppingbasket/examples/from_task_spec.py
@@ -0,0 +1,22 @@
+from shoppingbasket import ProductDB, Basket
+from shoppingbasket.promotions import MForN, MForNPounds
+
+# Setup basket with product database
+products = ProductDB.from_yaml('data/products.yaml')
+basket = Basket(products=products)
+
+# Add items
+basket.add_item_from_barcode(1)
+basket.add_item_from_barcode(1)
+basket.add_item_from_barcode(1)
+basket.add_item_from_barcode(4)
+basket.add_item_from_barcode(4)
+basket.add_item_from_barcode(5, 0.2)
+
+# Produce invoice with promotions
+promotions = [
+    MForN("Beans 3 for 2", {1}, m=3, n=2),
+    MForNPounds("Coke 2 for £1", {4}, m=2, n=1.0),
+]
+
+print(basket.generate_invoice(promotions=promotions).to_string())
