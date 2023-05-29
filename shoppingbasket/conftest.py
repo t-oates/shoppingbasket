c264bae302624505aa8cb36de8f1fabc153618e2
timbonator <me@timoates.com>
2023-05-29 21:40:26 +0100

    Removed extra test data file

--- shoppingbasket/conftest.py
+++ shoppingbasket/conftest.py
@@ -5,5 +5,5 @@ from shoppingbasket.product_db import ProductDB
 
 @pytest.fixture(scope='session')
 def products() -> ProductDB:
-    db_path = 'shoppingbasket/tests/data/full_product_db.yaml'
+    db_path = 'shoppingbasket/tests/data/products.yaml'
     return ProductDB.from_yaml(db_path)
--- shoppingbasket/tests/test_product_db.py
+++ shoppingbasket/tests/test_product_db.py
@@ -6,7 +6,8 @@ class TestProductDB:
         db_path = 'shoppingbasket/tests/data/products.yaml'
         product_db = ProductDB.from_yaml(db_path)
         expected_products = {
-            1: {'barcode': 1, 'name': 'Beans', 'unit_price': 1.5},
+            1: {'barcode': 1, 'name': 'Beans', 'unit_price': 0.50},
             2: {'barcode': 2, 'name': 'Onions', 'unit_price': 0.29, 'units': 'kg'},
         }
-        assert product_db._products == expected_products
+        assert product_db._products[1] == expected_products[1]
+        assert product_db._products[2] == expected_products[2]
