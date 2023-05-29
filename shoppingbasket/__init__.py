52f745791e3aee4e9c9eb7dd1a59de3b5cbb31be
timbonator <me@timoates.com>
2023-05-29 17:31:29 +0100

    added BasketItem, Discount and promotions to init file

--- shoppingbasket/__init__.py
+++ shoppingbasket/__init__.py
@@ -1,2 +1,4 @@
-from shoppingbasket.basket import Basket
+from shoppingbasket import promotions
+from shoppingbasket.basket import Basket, Invoice
+from shoppingbasket.basket_item import BasketItem, Discount
 from shoppingbasket.product_db import ProductDB
