155689346d1d455a7657af88555f2020fa4d5f31
timbonator <me@timoates.com>
2023-05-29 11:51:05 +0100

    Created test_basket_item.py

---
+++ tests/test_basket_item.py
@@ -0,0 +1,15 @@
+import pytest
+
+from basket_item import BasketItem
+
+
+class TestBasketItem:
+    @pytest.mark.parametrize(
+        'basket_item, expected_price', [
+            (BasketItem('Beans', 0.5), 0.5),
+            # 5.11 = 1.99 * 2.569 (rounded to 2dp)
+            (BasketItem('Onions', 1.99, units='kg', amount=2.569), 5.11),
+        ]
+    )
+    def test_price(self, basket_item, expected_price):
+        assert basket_item.line_price == expected_price
