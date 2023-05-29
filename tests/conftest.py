e746211ce7a177977f3e1a95fd4de00d06716228
timbonator <me@timoates.com>
2023-05-29 12:33:02 +0100

    Fixed incorrect declaration of products fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import pytest
 from product_db import ProductDB
 
 
-@pytest.fixture
-def products(scope='session'):
+@pytest.fixture(scope='session')
+def products() -> ProductDB:
     db_path = 'tests/db/full_product_db.yaml'
     return ProductDB.from_yaml(db_path)
